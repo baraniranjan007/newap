00a287fa944364c509a3319d009f2f3bdce7e574
baraniranjan007 <59719572+baraniranjan007@users.noreply.github.com>
2020-09-06 22:16:16 +0530

    Add files via upload

---
+++ build/precache-manifest.19c351da9553e0c49ee8d5c262e69082.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367700a9c80675a7b5c8ea42b9ea059d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1372971d0da9e7f6537e",
+    "url": "/static/js/2.4a3e98a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4a3e98a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d48bbce3d04e3143fed",
+    "url": "/static/js/main.723d1c3d.chunk.js"
+  },
+  {
+    "revision": "13e422b80bcb6f146d99",
+    "url": "/static/js/runtime-main.aeffcac1.js"
+  }
+]);
